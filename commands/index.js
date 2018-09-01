3ffa8132ae3d73e232a917fa53d83e8f0148b509
oxfn <oxfn@ya.ru>
2018-09-01 16:36:00 +0300

    Fixed commands/index

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,12 @@
 // commands/index.js
 
 module.exports = {
+  buyrambytes: require('./buyrambytes'),
   delegatebw: require('./delegatebw'),
   newaccount: require('./newaccount'),
+  refund: require('./refund'),
   transfer: require('./transfer'),
+  undelegatebw: require('./undelegatebw'),
   help: () => {
     console.log(`Usage: eoscmd <command> [params ...]`);
   }
