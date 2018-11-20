57ca922d565cd9593422416752f9c5744fc756d0
oxfn <oxfn@ya.ru>
2018-11-20 16:54:28 +0300

    Fixes

--- commands/delegatebw.js
+++ commands/delegatebw.js
@@ -1,13 +1,14 @@
 // commands/delegatebw.js
 
+const { formatAmount } = require('../utils');
 module.exports = async function delegatebw(config, api, params) {
   const { from, to, cpu, net } = params;
   return api.transaction((tx) => {
     tx.delegatebw({
       from: from,
       receiver: to,
-      stake_net_quantity: '0.1000 EOS',
-      stake_cpu_quantity: '0.1000 EOS',
+      stake_net_quantity: formatAmount(cpu | 0),
+      stake_cpu_quantity: formatAmount(net | 0),
       transfer: 0
     });
   });
--- commands/transfer.js
+++ commands/transfer.js
@@ -1,11 +1,13 @@
 // commands/transfer.js
 
+const { formatAmount } = require('../utils');
+
 module.exports = async function transfer(config, api, params) {
   const [from, to, amount, memo] = params;
   return api.transfer(
     from,
     to,
-    `${parseFloat(amount).toFixed(4)} EOS`,
+    formatAmount(amount),
     memo || ''
   );
 }
